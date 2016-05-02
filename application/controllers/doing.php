3d13b8491677129f1fe548db1b0d049e4096d0cc
Keven <zhao.k.f@qq.com>
2016-05-02 15:57:49 +0800

    index fun change

--- application/controllers/doing.php
+++ application/controllers/doing.php
@@ -9,6 +9,11 @@ class doing extends CI_Controller {
 
 	public function index()
 	{
+		echo "welcome baby :D";
+	}
+
+	public function testSqlite()
+	{
 		$data = array('uname' => 'test2','ukey' => 'test2key','email'=>'test2@localhost' );
 		$this->load->database();
 		$this->load->model('mdoing', 'mdo');
